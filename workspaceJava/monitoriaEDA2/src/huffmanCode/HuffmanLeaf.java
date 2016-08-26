dda5be4be5d95e25f296efe192ffc689453bc9bc
Guilherme <guilhermebaldissera@hotmail.com>
2016-08-25 21:54:31 -0300

    Initial commit with HuffmanCode example in C and Java

---
+++ workspaceJava/monitoriaEDA2/src/huffmanCode/HuffmanLeaf.java
@@ -0,0 +1,12 @@
+package huffmanCode;
+/*
+ * Classe do nó folha da Árvore 
+ */
+class HuffmanLeaf extends HuffmanTree {
+    public final char value; // A letra é atribuida a um nó folha 
+ 
+    public HuffmanLeaf(int freq, char val) {
+        super(freq);
+        value = val;
+    }
+}
---
+++ workspaceJava/monitoriaEDA2/src/huffmanCode/HuffmanNode.java
@@ -0,0 +1,14 @@
+package huffmanCode;
+
+/*
+ * Classe de um nó da Árvore 
+ */
+class HuffmanNode extends HuffmanTree {
+    public final HuffmanTree left, right; // sub-árvores
+ 
+    public HuffmanNode(HuffmanTree l, HuffmanTree r) {
+        super(l.frequency + r.frequency);
+        left = l;
+        right = r;
+    }
+}
